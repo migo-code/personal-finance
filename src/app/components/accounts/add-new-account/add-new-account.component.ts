f8ca6367e8c5d038bfaf22fc91a13caa64cd137b
migo-code <69990043+migo-code@users.noreply.github.com>
2021-11-13 00:58:24 -0800

    fix: eslint

--- src/app/components/accounts/add-new-account/add-new-account.component.ts
+++ src/app/components/accounts/add-new-account/add-new-account.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-add-new-account",
   templateUrl: "./add-new-account.component.html",
   styleUrls: ["./add-new-account.component.scss"],
 })
-export class AddNewAccountComponent implements OnInit {
+export class AddNewAccountComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
