eebc7b83551b6c46ff964962b0cfd21a87881d9c
migo-code <69990043+migo-code@users.noreply.github.com>
2021-11-13 10:22:26 -0800

    feature(transactions): adding firestore complex query

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { Transaction } from "../../../Transaction";
-import { Observable } from "rxjs";
-import { collection, collectionData, Firestore } from "@angular/fire/firestore";
+import { Transaction, Type } from "../../../Transaction";
+import { BehaviorSubject, combineLatest, Observable } from "rxjs";
+import {
+  Firestore,
+  QueryConstraint,
+  collection,
+  collectionData,
+  query,
+  where,
+} from "@angular/fire/firestore";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-transactions",
@@ -12,15 +20,40 @@ export class TransactionsComponent implements OnInit {
   displayedColumns: string[] = ["account.name", "date", "amount"];
   transactions: Transaction[] = [];
   transactionStuff$!: Observable<Transaction[]>;
+  typeFilter$: BehaviorSubject<number | null> = new BehaviorSubject<
+    number | null
+  >(null);
+  nameFilter$: BehaviorSubject<string | null> = new BehaviorSubject<
+    string | null
+  >(null);
 
   constructor(private firestore: Firestore) {}
 
   ngOnInit(): void {
-    this.transactionStuff$ = collectionData(
-      collection(this.firestore, "transactions")
-    ) as Observable<Transaction[]>;
+    const transactionsRef = collection(this.firestore, "transactions");
+    // TODO: `combineLatest` has been deprecated
+    this.transactionStuff$ = combineLatest(
+      this.typeFilter$,
+      this.nameFilter$
+    ).pipe(
+      switchMap(([type, name]) => {
+        let wheres: QueryConstraint[] = [];
+        if (type != null) {
+          wheres.push(where("type", "==", type));
+        }
+        if (name) {
+          wheres.push(where("name", "==", name));
+        }
+        return collectionData(query(transactionsRef, ...wheres)) as Observable<
+          Transaction[]
+        >;
+      })
+    );
+
     this.transactionStuff$.subscribe(
       (transactions) => (this.transactions = transactions)
     );
   }
+
+  onTypeFilter = (type: Type = Type.payment) => this.typeFilter$.next(type);
 }
